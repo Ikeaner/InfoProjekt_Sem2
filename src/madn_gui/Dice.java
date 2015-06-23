8302fd83410d90e41abdfde3d1f692697fe2750b
Jaqueline Timmermann <Jace2807@gmail.com>
2015-06-23 23:00:17 +0200

    hab kaputt hoffentlich wieder heile gemacht

--- src/madn_gui/Dice.java
+++ src/madn_gui/Dice.java
@@ -14,23 +14,14 @@ import javax.swing.JButton;
 @SuppressWarnings("serial")
 public class Dice extends JButton
 {
-<<<<<<< HEAD
-	//Bilder die beim Würfel angezeigt werden
-	ImageIcon i1 = new ImageIcon("src\\madn_images\\dice\\dice1.png");
-	ImageIcon i2 = new ImageIcon("src\\madn_images\\dice\\dice2.png");
-	ImageIcon i3 = new ImageIcon("src\\madn_images\\dice\\dice3.png");
-	ImageIcon i4 = new ImageIcon("src\\madn_images\\dice\\dice4.png");
-	ImageIcon i5 = new ImageIcon("src\\madn_images\\dice\\dice5.png");
-	ImageIcon i6 = new ImageIcon("src\\madn_images\\dice\\dice6.png");
-=======
+
 	ImageIcon i1 = new ImageIcon(getClass().getResource("/images/dice1.png"));
 	ImageIcon i2 = new ImageIcon(getClass().getResource("/images/dice2.png"));
 	ImageIcon i3 = new ImageIcon(getClass().getResource("/images/dice3.png"));
 	ImageIcon i4 = new ImageIcon(getClass().getResource("/images/dice4.png"));
 	ImageIcon i5 = new ImageIcon(getClass().getResource("/images/dice5.png"));
 	ImageIcon i6 = new ImageIcon(getClass().getResource("/images/dice6.png"));
->>>>>>> f85fcfb0c1fbe5f4074c3929100d3815db6851b0
-	
+
 	public Dice()
 	{
 		//setzt als Anfangsbild das Bild mit der 1
